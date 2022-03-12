51d3e3a7940a0ff5cc5a7002aeeeb1a252e5e8cd
Mae Pena <maepena@Maes-MacBook-Pro.local>
2022-03-12 08:43:11 -0800

    adding bootstrap grid to index page

--- views/home.jsx
+++ views/home.jsx
@@ -8,6 +8,13 @@ function home () {
               <h1>HOME</h1>
               <a href="/places">
   <button className="btn-primary">Places Page</button>
+  <div>
+    <img src="/images/pizza.jpg" alt="pizza" />
+  </div>
+  <div>
+  Photo by <a href="https://unsplash.com/@iavnt?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Ivan Torres</a> on <a href="https://unsplash.com/s/photos/pizza?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
+  
+  </div>
 </a>
           </main>
       </Def>
