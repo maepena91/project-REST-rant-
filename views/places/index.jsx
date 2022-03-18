31588474f609da1d980dd1e0b92cbf2329eeab97
Mae Pena <maepena@Maes-MacBook-Pro.local>
2022-03-17 22:26:50 -0700

    update

--- views/places/index.jsx
+++ views/places/index.jsx
@@ -1,37 +1,29 @@
-// Modules and Globals
-require('dotenv').config()
-const express = require('express')
-const methodOverride = require('method-override')
+const React = require("react");
+const Def = require("../default");
 
-function index (data) {
+function index(data) {
   let placesFormatted = data.places.map((place, index) => {
     return (
-      <div className="col-sm-6">
+      <div className='col-sm-6'>
         <h2>
-          <a href={`/places/${index}`} >
-            {place.name}
-          </a>
+          <a href={`/places/${index}`}>{place.name}</a>
         </h2>
-        <p className="text-center">
-          {place.cuisines}
-        </p>
+        <p className='text-center'>{place.cuisines}</p>
         <img src={place.pic} alt={place.name} />
-        <p className="text-center">
+        <p className='text-center'>
           Located in {place.city}, {place.state}
         </p>
       </div>
-    )
-  })
-    return (
-      <Def>
-          <main>
-              <h1>Places to Rant</h1>
-              <div className="row">
-              {placesFormatted}
-              </div>
-          </main>
-      </Def>
-  )
-  }
+    );
+  });
+  return (
+    <Def>
+      <main>
+        <h1>REST-rant</h1>
+        <div className='row'>{placesFormatted}</div>
+      </main>
+    </Def>
+  );
+}
 
-  module.exports = index
+module.exports = index;
