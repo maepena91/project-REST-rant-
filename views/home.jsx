5fb013a663060a3105a6a373530e2e40210616a9
Mae Pena <maepena@Maes-MacBook-Pro.local>
2022-03-24 17:44:55 -0700

    update comments

--- views/home.jsx
+++ views/home.jsx
@@ -6,13 +6,10 @@ function home () {
         <Def>
             <main>
     <h1>HOME</h1>
-    <div>
-      <img src="/images/Drinks.jpg" alt="drinks" />
+      <img src="/images/Drinks.jpg" alt="Drinks" />
       <div>
       Photo by <a href="https://unsplash.com/@ksousaphotography?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Katherine Sousa</a> on <a href="https://unsplash.com/s/photos/drinks?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
-    
-  </div>
-    </div>
+</div>
     <a href="/places">
         <button className="btn-primary">Places Page</button>
     </a>
